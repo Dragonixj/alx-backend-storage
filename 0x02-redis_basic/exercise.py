838a81db3cf88234a7df1b923e9c127af5afebff
Dragonixj <delafiado@gmail.com>
2024-05-01 22:38:55 +0000

    task 0(corrections)

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,10 +12,11 @@ class Cache:
     """Represents an object for storing in a Redis data storage"""
 
     def __init__(self) -> None:
+        """init a private redis instance and flush the database"""
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data: Union[str, int, bytes, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores a value in a Redis data storage and returns the key"""
         data_key = str(uuid.uuid4())
         self._redis.set(data_key, data)
